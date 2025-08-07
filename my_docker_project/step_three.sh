58695bf678626ff9a294e18df539b71afb555d43
aaravdinesh <aarav.dinesh@gmail.com>
2025-08-06 20:03:39 -0400

    Update step_three.sh

--- my_docker_project/step_three.sh
+++ my_docker_project/step_three.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Find all unique versions with -SNAPSHOT
 versions=$(grep -rohP '<version>\K[0-9A-Za-z\.\-]+(?=-SNAPSHOT</version>)' --include=pom.xml . | sort -u)
 
 for version in $versions; do
-	  read -p "Replace ${version}-SNAPSHOT with what? " stable_ver
-	  find . -name "pom.xml" -exec sed -i.bak "s/${version}-SNAPSHOT/${stable_ver}/g" {} \;
-	  echo "${version}-SNAPSHOT has been replaced with ${stable_ver}"
+    # Replace only the -SNAPSHOT part, keeping the version
+    find . -name "pom.xml" -exec sed -i.bak "s/${version}-SNAPSHOT/${version}/g" {} \;
+    echo "Replaced ${version}-SNAPSHOT with ${version}"
 done
